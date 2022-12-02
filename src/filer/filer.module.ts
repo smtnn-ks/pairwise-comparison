2e15f71d316c23cf823cb068612cb871cb790554
Константин Сметанин <smtnn.ks@gmail.com>
2022-12-02 17:16:43 +0300

    filer.module deleted

--- src/filer/filer.module.ts
+++ src/filer/filer.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilerService } from './filer.service';
-import { FilerController } from './filer.controller';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
   providers: [FilerService],
-  controllers: [FilerController],
   exports: [FilerService],
 })
 export class FilerModule {}
